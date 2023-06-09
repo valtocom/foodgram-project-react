1ef4aebb3012ede27b7ba1be6437d72b6fa9d12b
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-09 07:48:05 +0300

    add workflows

--- backend/foodgram/recipes/management/commands/fillbase.py
+++ backend/foodgram/recipes/management/commands/fillbase.py
@@ -1,5 +1,4 @@
 import csv
-import os
 
 from django.core.management.base import BaseCommand
 
@@ -11,18 +10,17 @@ class Command(BaseCommand):
 
     help = 'Перевод из csv файлов в модели проекта'
 
-    def fill_ingredient(self):
-        """Заполнение модели Ingredient."""
-        with open(
-            os.path.join('ingredients.csv'),
-            'r', encoding='utf-8'
-        ) as csv_file:
-            data = csv.DictReader(csv_file)
-            for item in data:
-                Ingredient.objects.get_or_create(
-                    name=item['name'],
-                    measurement_unit=item['measurement_unit']
-                )
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
 
     def handle(self, *args, **options):
-        self.fill_ingredient()
+        file_path = options['path']
+        with open(file_path, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                Ingredient.objects.create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
+
