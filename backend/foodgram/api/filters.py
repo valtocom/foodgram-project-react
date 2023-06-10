d3cca26677f400cf86035fd99de0b68e6de8c16b
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-11 01:12:24 +0300

    fix comments

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -18,16 +18,21 @@ class RecipeFilter(FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
+        fields = (
+            'author', 'tags',
+            'is_favorited', 'is_in_shopping_cart'
+        )
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(favorite_recipe__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(carts__user=self.request.user)
+            return queryset.filter(
+                recipe_shopping_cart__user=self.request.user
+            )
         return queryset
 
 
