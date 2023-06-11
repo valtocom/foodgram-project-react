043041b4f910a3f6f7a01c7124f205c055295b83
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-11 03:26:01 +0300

    fix comments

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -18,7 +18,6 @@ class TagAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'measurement_unit')
     search_fields = ('name',)
-    list_filter = ('name',)
     empty_value_display = settings.EMPTY_VALUE
 
 
