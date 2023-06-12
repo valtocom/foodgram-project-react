fbc1fb6fa4a1844b87e91254d6afad5eb5d6d2f6
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-12 17:57:08 +0300

    fix comments

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -10,3 +10,20 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('username', 'email')
     empty_value_display = settings.EMPTY_VALUE
+    fieldsets = (
+        (None, {"fields": ("username")}),
+        (('Personal info'), {"fields": ("first_name", "last_name", "email")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
