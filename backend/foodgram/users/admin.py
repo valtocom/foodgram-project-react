cf647d9f7dbeafc5321bf9df01dae509a39bead1
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-12 17:09:16 +0300

    fix comments

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.contrib import admin, get_user_model
+from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
