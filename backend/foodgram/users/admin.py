6cb86e7fe2973acf3fc5f4cb64c5144b59dc9990
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-12 18:33:25 +0300

    fix comments

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -8,3 +8,19 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'email', 'username', 'first_name', 'last_name')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('username', 'email')
+    fieldsets = (
+    ('Общая информация', {'fields': ('first_name', 'last_name', 'email')}),
+    (
+        'Права доступа',
+        {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        },
+    ),
+    ('Даты', {'fields': ('last_login')}),
+)
