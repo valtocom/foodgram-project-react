23270767279ba47719550e90c04447d702587d3c
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-12 17:41:12 +0300

    fix comments

--- backend/foodgram/users/apps.py
+++ backend/foodgram/users/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
     name = 'users'
     verbose_name = 'Управление пользователями'
