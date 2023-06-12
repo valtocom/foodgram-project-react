5b694bc13f9d151b65fa83a3d468ad37153ae780
Vladimir Dmitriev <gdvsbook@gmail.com>
2023-06-12 18:42:44 +0300

    fix comments

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,26 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'email', 'username', 'first_name', 'last_name')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('username', 'email')
+class UserAdmin(UserAdmin):
     fieldsets = (
-    ('Общая информация', {'fields': ('first_name', 'last_name', 'email')}),
-    (
-        'Права доступа',
-        {
-            'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
-                'groups',
-                'user_permissions',
-            ),
-        },
-    ),
-    ('Даты', {'fields': ('last_login')}),
-)
+        (None, {'fields': ('username', 'password')}),
+        ('Общая информация', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Права доступа', {
+            'fields': ('is_active', 'is_staff',
+                       'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Даты', {'fields': ('last_login',)}),
+    )
